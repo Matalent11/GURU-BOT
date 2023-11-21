bcd6ec68536c7a662c11479606a1785d7f842a41
Matalent11 <151641626+Matalent11@users.noreply.github.com>
2023-11-21 18:52:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = ['255676644789', 'Matalent bot',true],
-  ['917605902011', 'GURU', true],
+  ['917605902011', 'Matalent Bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
