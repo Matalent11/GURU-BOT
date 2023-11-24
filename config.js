a093a216d6a7510e684e34d16ab061e072064fb5
Matalent11 <151641626+Matalent11@users.noreply.github.com>
2023-11-24 16:52:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,8 @@ import axios from 'axios'
 
 
 global.owner = ['255676644789', 'Matalent',true],
-  ['917605902011', 'Matalent Bot', true],
-  ['6281255369012', 'Ash', true], 
-  [''] 
-] //Number of owners
-
-//global.pairingNumber = "" //put your bot number here
+Number of owners
+global.pairingNumber = "" //put your bot number here
 
 global.mods = ['917605902011','32460248586'] 
 global.prems = ['917605902011', '32460248586', '919398758484']
@@ -50,10 +46,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'matalent bot'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'matalent' 
+global.author = 'matalent11' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
